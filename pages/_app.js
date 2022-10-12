045639652c04832a761557930a09556358541993
Matt TK Taylor <matt@tk.gg>
2022-10-13 00:35:04 +0100

    add plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "theme-ui";
 import theme from "../theme.js";
+import PlausibleProvider from "next-plausible";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <PlausibleProvider
+      domain="tk.gg"
+      customDomain="https://stats.tk.gg"
+      selfHosted={true}
+    >
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </PlausibleProvider>
   );
 }
 
