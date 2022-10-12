57523199ef18ed5ea74d83a3d51eeabbfebc8e86
Matt TK Taylor <matt@tk.gg>
2022-10-13 00:43:46 +0100

    correct domain

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,18 @@ import "../styles/globals.css";
 import { ThemeProvider } from "theme-ui";
 import theme from "../theme.js";
 import PlausibleProvider from "next-plausible";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <PlausibleProvider
-      domain="tk.gg"
+      domain="ror.tk.gg"
       customDomain="https://stats.tk.gg"
       selfHosted={true}
     >
+      <Head>
+        <link rel="icon" href="/images/Artifact_of_Command.pn" />
+      </Head>
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
