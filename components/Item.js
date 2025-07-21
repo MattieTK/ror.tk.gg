f3fbb2cfbe86dc22b35445d530ed75199dbf0a9b
Matt Taylor <matt@tk.gg>
2025-07-21 23:33:10 +0100

    Update for Seekers of the Storm

--- components/Item.js
+++ components/Item.js
@@ -1,55 +1,56 @@
-import { Box, Image } from "theme-ui";
-import { useState } from "react";
+import { Box, Image } from 'theme-ui';
+import { useState } from 'react';
 
 const Item = ({
-  image,
-  name,
-  description,
-  classes,
-  setHoveredItem,
-  position,
+	image,
+	name,
+	description,
+	classes,
+	setHoveredItem,
+	position,
 }) => {
-  const [hover, setHover] = useState(false);
+	const [hover, setHover] = useState(false);
 
-  return (
-    <Box
-      data-image={image}
-      data-name={name}
-      data-description={description}
-      onMouseEnter={e => {
-        setHover(true);
-        setHoveredItem({
-          name,
-          description,
-          classes,
-          image,
-        });
-        console.log("enter", position);
-      }}
-      onMouseOut={e => {
-        if (hover) {
-          setHoveredItem(null);
-          // console.log("out!", e);
-          setHover(false);
-        } else {
-        }
-      }}
-      sx={{
-        border: "#7e7f7f 1px solid",
-        margin: "4px",
-        padding: "2px",
-        height: "128px",
-        backgroundImage: `url(/images/${image})`,
-        backgroundSize: "contain",
-        backgroundRepeat: "no-repeat",
-      }}
-    >
-      {/* <Image
+	return (
+		<Box
+			data-image={image}
+			data-name={name}
+			data-description={description}
+			onMouseEnter={e => {
+				setHover(true);
+				setHoveredItem({
+					name,
+					description,
+					classes,
+					image,
+				});
+				console.log('enter', position);
+			}}
+			onMouseOut={e => {
+				if (hover) {
+					setHoveredItem(null);
+					// console.log("out!", e);
+					setHover(false);
+				} else {
+				}
+			}}
+			sx={{
+				border: '#7e7f7f 1px solid',
+				margin: '4px',
+				padding: '2px',
+				height: '128px',
+				backgroundImage: `url(/images/${image})`,
+				backgroundSize: 'contain',
+				backgroundRepeat: 'no-repeat',
+			}}
+		>
+			{/* <Image
         sx={{ width: "inherit", height: "auto" }}
         src={`images/${image}`}
       ></Image> */}
-    </Box>
-  );
+			<p>{position}</p>
+		</Box>
+	);
 };
 
 export default Item;
